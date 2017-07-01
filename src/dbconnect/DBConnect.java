7f81364cc16463bc5cc51996ab73f076784dbb41
qc1991329 <qc1991329@163.com>
2017-07-01 12:40:08 +0800

    更新报表逻辑

--- src/dbconnect/DBConnect.java
+++ src/dbconnect/DBConnect.java
@@ -1,8 +1,8 @@
 package dbconnect;
 
 import bean.CityWorkingTime;
-import bean.JangSuVehTotal;
-import bean.Vehtotal;
+import bean.MonthSalCount;
+import bean.VehModelCount;
 import com.google.gson.Gson;
 import com.taobao.api.ApiException;
 import com.taobao.api.DefaultTaobaoClient;
@@ -19,7 +19,7 @@ import java.util.Calendar;
 
 public class DBConnect {
 
-	public String getVehtotal() {
+	/*public String getVehtotal() {
 		ResultSet rs = null;
 		Statement stmt = null;
 		Connection conn = null;
@@ -28,7 +28,7 @@ public class DBConnect {
 		try {
 			conn = GetConnect.getconnect();
 			stmt = conn.createStatement();
-			String sqlvehtotsl = 
+			String sqlvehtotsl =
 					"select at_province,total from (select t.at_province,sum(t.at_total) as total from AREA_VEHICLE_TOTAL t where t.at_year = 2017 and t.at_province is not null  group by t.at_province order by total DESC) where rownum<=10";
 			rs = stmt.executeQuery(sqlvehtotsl);
 			while (rs.next()) {
@@ -46,23 +46,120 @@ public class DBConnect {
 		} finally {
 				closeconnect(rs,stmt,conn);
 		}
+	}*/
 
+	public String getVehtotal() {
+		ResultSet rs = null;
+		Statement stmt = null;
+		Connection conn = null;
+		Gson gson = new Gson();
+		ArrayList<VehModelCount> veList  =  new ArrayList<VehModelCount>();
+		try {
+			conn = GetConnect.getconnect();
+			stmt = conn.createStatement();
+			String sqlvehtotsl =
+					"select vmi_name ,salcount from (select  m.vmi_id ,m.vmi_name,count(*) as salcount from vehicle_info t join vehicle_model_info m on t.vi_model = m.vmi_id\n" +
+							"where t.vi_own_type = 3 and t.vi_is_deleted = 0 \n" +
+							"and t.vi_sale_time between to_date('2017-1-01','yyyy-mm-dd') and to_date('2017-12-31','yyyy-mm-dd')\n" +
+							"group by m.vmi_id,m.vmi_name\n" +
+							"order by salcount DESC) where rownum <= 10";
+			rs = stmt.executeQuery(sqlvehtotsl);
+			while (rs.next()) {
+				VehModelCount veobject = new VehModelCount(rs.getString("vmi_name"),rs.getInt("salcount"));
+					veList.add(veobject);
+			}
+			String jsonstring = gson.toJson(veList);
+			return jsonstring;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			closeconnect(rs,stmt,conn);
+		}
 	}
+
+
+	/*public String getVehtotal() {
+		ResultSet rs = null;
+		Statement stmt = null;
+		Connection conn = null;
+		Gson gson = new Gson();
+		ArrayList<WorkingRate> veList  =  new ArrayList<WorkingRate>();
+		try {
+			conn = GetConnect.getconnect();
+			stmt = conn.createStatement();
+			String sqlvehtotsl = "WITH TEMP AS\n" +
+					" (SELECT OOD_PROVINCE AS REGION,\n" +
+					"         ONOFF.VI_ID,\n" +
+					"         SUM(OOD_WORKHOURS) AS TOTALWORKHOURS\n" +
+					"    FROM ON_OFF_DAY201706 ONOFF\n" +
+					"   WHERE 1=1\n" +
+					"     AND OOD_PROVINCE IS NOT NULL\n" +
+					"     AND VI_OWN_TYPE = 3\n" +
+					"     AND OOD_PROVINCE <> 'null'\n" +
+					"   GROUP BY OOD_PROVINCE, ONOFF.VI_ID\n" +
+					"   ),\n" +
+					"VEHICLESTATS AS\n" +
+					" (SELECT REGION,VI_TONNAGE_TYPE, VEHICLE.VI_ID, SUM(TOTALWORKHOURS) AS TOTALWORKHOURS\n" +
+					"    FROM TEMP\n" +
+					"    JOIN VEHICLE_INFO VEHICLE\n" +
+					"      ON TEMP.VI_ID = VEHICLE.VI_ID\n" +
+					"   WHERE 1 = 1\n" +
+					"     AND VEHICLE.VI_IS_DELETED = 0\n" +
+					"     AND VI_TERMINAL_MODEL IN\n" +
+					"         (SELECT TMM_ID\n" +
+					"            FROM TERMINAL_MODEL_INFO\n" +
+					"           WHERE TERMINAL_MODEL_INFO.TMM_IS_RELAY = 0)\n" +
+					"          AND VEHICLE.VI_SALE_TIME is not null\n" +
+					"   GROUP BY REGION,VEHICLE.VI_TONNAGE_TYPE, VEHICLE.VI_ID)\n" +
+					"  select PR_NAME,totalWorkHours,VEHICLECOUNT,trunc(rate,2) as rate from (SELECT PR_NAME,\n" +
+					"       MAX(PR_CODE) PR_CODE,\n" +
+					"          SUM(TOTALWORKHOURS) AS totalWorkHours,          \n" +
+					"       COUNT(*) AS VEHICLECOUNT,\n" +
+					"           SUM(TOTALWORKHOURS) /COUNT(*) AS  rate\n" +
+					"  FROM   PROVINCE_INFO   \n" +
+					"  LEFT JOIN VEHICLESTATS ON PR_NAME = REGION\n" +
+					" WHERE 1=1\n" +
+					"  AND pr_name<>'null'\n" +
+					" GROUP BY PR_NAME\n" +
+					" having SUM(TOTALWORKHOURS)>0  \n" +
+					" ORDER BY totalWorkHours DESC) where rownum <= 10";
+			rs = stmt.executeQuery(sqlvehtotsl);
+			while (rs.next()) {
+				WorkingRate workingRate = new WorkingRate(rs.getString("PR_NAME"),rs.getDouble("totalWorkHours")/1000,rs.getInt("VEHICLECOUNT")/10,rs.getDouble("rate"));
+				veList.add(workingRate);
+			}
+			String jsonstring = gson.toJson(veList);
+			return jsonstring;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			closeconnect(rs,stmt,conn);
+		}
+
+	}*/
 	
 	public String getJangsuVehTotal(){
 		ResultSet rs = null;
 		Statement stmt = null;
 		Connection conn = null;
 		Gson gson = new Gson();
-		ArrayList<JangSuVehTotal> jsArrayList = new ArrayList<JangSuVehTotal>();
+		ArrayList<MonthSalCount> jsArrayList = new ArrayList<MonthSalCount>();
 		try {
 			conn = GetConnect.getconnect();
 			stmt = conn.createStatement();
 			String sqlvehtotsl = 
-					"select t.at_year,t.at_province,sum(t.at_total) as total from AREA_VEHICLE_TOTAL t where t.at_province = '江苏' group by t.at_year,t.at_province order by t.at_year";
+					"select to_number(mon) as mon, sum(salcount) as salcount  \n" +
+							"from (select to_char(t.vi_sale_time,'mm') as mon , count(*) as salcount  \n" +
+							"from vehicle_info t where t.vi_own_type = 3 and t.vi_is_deleted = 0 and t.vi_sale_time is not null and t.vi_sale_time between to_date('2017-1-01','yyyy-mm-dd') and to_date('2017-12-31','yyyy-mm-dd')\n" +
+							"group by t.vi_sale_time \n" +
+							"order by t.vi_sale_time)\n" +
+							"group by mon\n" +
+							"order by mon";
 			rs = stmt.executeQuery(sqlvehtotsl);
 			while (rs.next()) {
-				JangSuVehTotal jangSuVehTotal = new JangSuVehTotal(rs.getInt("at_year"),rs.getInt("total"));
+				MonthSalCount jangSuVehTotal = new MonthSalCount(rs.getInt("mon")+"月",rs.getInt("salcount"));
 				jsArrayList.add(jangSuVehTotal);
 			
 			}
